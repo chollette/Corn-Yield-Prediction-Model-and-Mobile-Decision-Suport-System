9ad5aa731b2f56965cc8eed4309baae7be9e62ae
Chollette Olisah <choolisah@gmail.com>
2021-06-11 11:08:31 +0100

    Add files via upload

---
+++ gcrf-farmer-flask-app/app.py
@@ -0,0 +1,17 @@
+import pickle
+from flask import Flask, request
+#from flask import jsonify
+from model_files.score import predict_yield
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def predict():
+	data = request.get_json()
+	with open('./model_files/dfr_model.bin', 'rb') as model_in:
+		model = pickle.load(model_in)
+		model_in.close()
+	result = predict_yield(data, model)
+	return result.tolist()
+	#return jsonify(result)
+
