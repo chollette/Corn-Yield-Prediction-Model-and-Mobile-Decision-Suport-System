87fbf900213010411d3ca559b375065e3c71c1d4
Chollette Olisah <choolisah@gmail.com>
2021-06-13 08:35:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import sys
 import logging
 from flask import Flask, request
 from flask import jsonify
-from score import predict_yield
+from model_files.score import predict_yield
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def predict():
 	data = request.get_json()
-	with open('dfr_model.bin', 'rb') as model_in:
+	with open('./model_files/dfr_model.bin', 'rb') as model_in:
 		model = pickle.load(model_in)
 		model_in.close()
 	result = predict_yield(data, model)
