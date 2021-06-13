05623a9f8edf7a37e9f38d0df280b501592c8bf0
Chollette Olisah <choolisah@gmail.com>
2021-06-13 10:30:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import pickle
+import joblib
 import sys
 import logging
 from flask import Flask, request
 from flask import jsonify
-from model_files.score import predict_yield
+from outputs.score import predict_yield
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def predict():
 	data = request.get_json()
-	with open('./model_files/dfr_model.pickle', 'rb') as model_in:
-		model = pickle.load(model_in)
-		model_in.close()
+	model_path = './outputs/model.joblib'
+        model = joblib.load(model_path)
 	result = predict_yield(data, model)
 	#return result.tolist()
 	return jsonify(result)
